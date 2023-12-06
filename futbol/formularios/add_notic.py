fd57e296a35e759b872ad2f5bbcdf20cbb05a8f0
MarvinTrey5 <santosmarvin201@gmail.com>
2023-12-06 08:10:22 -0600

    Formulario Para La Tabla Noticias

--- futbol/formularios/add_notic.py
+++ futbol/formularios/add_notic.py
@@ -1,5 +1,6 @@
 from django import forms
 # Tabla de noticias
+# Para la visualización en el form.
 class Add_notic(forms.Form):
     resultado=forms.CharField(max_length=50)
     encuentros=forms.CharField(max_length=100)
