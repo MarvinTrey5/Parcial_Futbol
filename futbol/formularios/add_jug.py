726242f5af9400acb7b63c09d2b3f08784a7be9e
MarvinTrey5 <santosmarvin201@gmail.com>
2023-12-06 07:29:54 -0600

    Forms De Añadir jugar utilizando ModelChoiceField

--- futbol/formularios/add_jug.py
+++ futbol/formularios/add_jug.py
@@ -1,4 +1,7 @@
 from django import forms
+# Se importa la app que almacena los modelos 
+# y se importa la tabla/clase a la que está relacionada
+# en este caso equipos.
 from futbol.models import Equipos
 
 # Tabla de jugadores
@@ -7,4 +10,5 @@ class Add_jugad(forms.Form):
     apellido=forms.CharField(max_length=100)
     nacionalidad=forms.CharField(max_length=100)
     fecha_nacimiento=forms.DateField()
+    # Se utilizó el ModelchoiceField para obtener todos los elementos del id de la tabla relaciona Equipos.
     pertenece_id=forms.ModelChoiceField(queryset=Equipos.objects.all(),empty_label=None, to_field_name="id")
